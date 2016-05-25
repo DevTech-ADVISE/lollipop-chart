460a6aac2acbd8f467cc0b181260bfa8ef7264c0
Andrew Blowe <tehandyb@gmail.com>
2016-05-25 17:02:35 -0400

    most tests are added, and a lot of the chart calculation functionality is in place

--- demo/index.js
+++ demo/index.js
@@ -1,14 +1,17 @@
-var lollipopChart = LollipopChart('#lollipop-example');
-var data = [
-  {country: 'Afghanistan', value: '444', average: '122'},
-  {country: 'Bangladesh', value: '206', average: '122'},
-  {country: 'Bhutan', value: '4', average: '122'},
-  {country: 'India', value: '193', average: '122'},
-  {country: 'Maldives', value: '11', average: '122'},
-  {country: 'Nepal', value: '155', average: '122'},
-  {country: 'Pakistan', value: '310', average: '122'},
-  {country: 'Sri Lanka', value: '113', average: '122'},
-];
+var lollipopChart = LollipopChart('#myLollipopChart');
+var data = {
+  min: 15,
+  max: 100,
+  members: [
+    {country: 'Afghanistan', value: '80', average: '60'},
+    {country: 'Bangladesh', value: '50', average: '50'},
+    {country: 'Bhutan', value: '15', average: '35'},
+    {country: 'India', value: '100', average: '55'},
+    {country: 'Maldives', value: '25', average: '30'},
+    {country: 'Nepal', value: '35', average: '40'},
+    {country: 'Pakistan', value: '50', average: '60'},
+    {country: 'Sri Lanka', value: '65', average: '50'},
+  ]};
 
 lollipopChart
   .width(200)
