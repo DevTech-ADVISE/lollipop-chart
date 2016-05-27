b3479677b7df199afda01311bc860fc991ea1313
Andrew Blowe <tehandyb@gmail.com>
2016-05-27 12:36:26 -0400

    chart will not override scale if it gets set by the developer. also cleaned up a few lines that weren't used anymore

--- demo/index.js
+++ demo/index.js
@@ -1,19 +1,20 @@
 var lollipopChart = LollipopChart('#myLollipopChart');
 var data = [
-    {country: 'Afghanistan', value: 80, average: 60, scale: d3.scale.linear().domain([0, 300])},
-    {country: 'Bangladesh', value: 50, average: 50, scale: d3.scale.linear().domain([40, 100])},
-    {country: 'Bhutan', value: 15, average: 15, scale: d3.scale.linear().domain([0, 15])},
-    {country: 'India', value: 100, average: 100, scale: d3.scale.linear().domain([0, 100])},
-    {country: 'Maldives', value: 25, average: 30, scale: d3.scale.linear().domain([10, 40])},
-    {country: 'Nepal', value: 37, average: 40, scale: d3.scale.linear().domain([10, 60])},
-    {country: 'Pakistan', value: 53, average: 60, scale: d3.scale.linear().domain([0, 100])},
-    {country: 'Sri Lanka', value: 40, average: 65, scale: d3.scale.linear().domain([40, 200])},
+    {country: 'Afghanistan', value: 80, average: 60},
+    {country: 'Bangladesh', value: 50, average: 50},
+    {country: 'Bhutan', value: 15, average: 15},
+    {country: 'India', value: 100, average: 100},
+    {country: 'Maldives', value: 25, average: 30},
+    {country: 'Nepal', value: 37, average: 40},
+    {country: 'Pakistan', value: 53, average: 60},
+    {country: 'Sri Lanka', value: 40, average: 65},
   ];
 
 // Chart setup
 lollipopChart
   .nameAccessor(function(d) { return d.country; })
-  .comparisonValueAccessor(function(d) { return d.average; });
+  .comparisonValueAccessor(function(d) { return d.average; })
+  .yScale(d3.scale.linear().domain([0, 100]))
   
 // Chart render 
 lollipopChart.render(data);
